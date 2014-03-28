0f4b67f3c77ac7dd99986ae6d1edd91cab85325f
vkuzma <kuzma@allink.ch>
2014-03-28 20:35:30 +0100

    working on gui

---
+++ Assets/Scripts/StartGuiController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGuiController : MonoBehaviour {
+
+	float screenCenter;
+	float btnWidth;
+	float btnHeight;
+
+	// Use this for initialization
+	void Start () {
+		btnWidth = 150;
+		btnHeight = 50;
+		screenCenter = Screen.width / 2 - btnWidth  / 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI () {
+		if (GUI.Button (new Rect (screenCenter, 10, btnWidth, btnHeight), "Start Game")) {
+			Application.LoadLevel("Scene1");
+		}
+
+		if (GUI.Button (new Rect (screenCenter, 2 * (10 + btnHeight), btnWidth, btnHeight), "Settings")) {
+			Application.LoadLevel("Scene1");
+		}
+
+		if (GUI.Button (new Rect (screenCenter, 3 * (10 + btnHeight), btnWidth, btnHeight), "Quit Game")) {
+			Application.LoadLevel("Scene1");
+		}
+	}
+}
